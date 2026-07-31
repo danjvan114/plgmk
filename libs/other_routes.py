3a9f6ae12d70fc892417bf144f0d85b8b99a8d5f
danjvan114 <danjvan114@github.com>
2026-07-31 21:40:54 +0800

    user

--- libs/other_routes.py
+++ libs/other_routes.py
@@ -216,7 +216,7 @@ def register_other_routes():
     def mc_register():
         return render_root_template('mcreg.html')
 
-    @app.route('/mc/profile')
+    @app.route('/app/user')
     def mc_profile():
         return send_from_directory(os.path.join(os.path.dirname(os.path.dirname(__file__)), 
                                               'webapp', 'mcst'), 
