adfb7a98088ba5e8724751a47bc69fbc0d46f7fb
danjvan114 <danjvan114@github.com>
2026-08-02 16:58:49 +0800

    bug

--- libs/mc_routes.py
+++ libs/mc_routes.py
@@ -4,6 +4,7 @@ from .invite_code import get_invite_manager
 from .database import get_player_engine
 from sqlalchemy import text
 import hashlib
+import oss's
 
 def mc_get_invite_code():
     print(f"DEBUG mc_get_invite_code: Function called")
